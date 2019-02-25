863a6108d567bff204de885292dcf8c000e88808
Jean Santos <jeansantosc@gmail.com>
2019-02-25 12:52:37 -0300

    add middleware to handle user authentication by jwt

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const routes = require("express").Router();
 
+const authMiddleware = require("./app/middleware/auth");
+
 const SessionController = require("./app/controllers/SessionController");
 
 routes.post("/sessions", SessionController.store);
+
+// only applies to routes after this declaration
+routes.use(authMiddleware);
 
 routes.get("/dashboard", (req, res) => {
   return res.status(200).send();
