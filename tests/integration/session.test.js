6b7c5f93d00aee041b81c3acbdd37ea6eb6d4576
Jean Santos <jeansantosc@gmail.com>
2019-02-25 13:06:33 -0300

    add test to reject access to route if invalid jwt token

--- tests/integration/session.test.js
+++ tests/integration/session.test.js
@@ -66,8 +66,16 @@ describe("Authentication", () => {
     expect(response.status).toBe(200);
   });
 
-  it("should not be able to access private routes when not authenticated", async () => {
+  it("should not be able to access private routes without jwt token", async () => {
     const response = await request(app).get("/dashboard");
+
+    expect(response.status).toBe(401);
+  });
+
+  it("should not be able to access private routes with invalid jwt token", async () => {
+    const response = await request(app)
+      .get("/dashboard")
+      .set("Authorization", "Bearer 123123");
 
     expect(response.status).toBe(401);
   });
